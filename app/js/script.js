d582759591ca55825dd2fe321f440d2d7375902e
Vladimir Lipodat <sibi_7@mail.ru>
2017-04-05 11:33:02 +0300

    section home

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
+    $(".slider").HSlider({
+// jquery easing options
+        easing:"ease",
 
+// animation speed
+        animationTime:800,
+
+// display side navigation
+        pagination:false,
+
+// display description
+        description:true
+    });
 });
+
